c08d9c8220dfa0d77b4e238c539958f0e4bb7ac6
nskazki <nskazki@gmail.com>
2019-10-20 06:58:00 +0300

    Setup eslint

--- example/index.js
+++ example/index.js
@@ -1,39 +1,39 @@
 'use strict'
 
-var MyLogger = require('../');
-var colors = require('colors');
+const MyLogger = require('../')
+const colors = require('colors')
 
-var infoLogger = MyLogger.Informer("permitLogServer")
-var errorLogger = MyLogger.Panic("permitLogServer")
-var warnLogger = MyLogger.Warning("permitLogServer")
-var debugLogger = MyLogger.CusotomLogger("permitLogServer", "DEBG", colors.cyan)
+const infoLogger = MyLogger.Informer('permitLogServer')
+const errorLogger = MyLogger.Panic('permitLogServer')
+const warnLogger = MyLogger.Warning('permitLogServer')
+const debugLogger = MyLogger.CusotomLogger('permitLogServer', 'DEBG', colors.cyan)
 
 infoLogger('main - info', {
-	desc: 'hi, I\'m a friendly line',
-	isAllOk: true,
-	amIFine: true,
-	bestDay: new Date()
-});
+  desc: 'hi, I\'m a friendly line',
+  isAllOk: true,
+  amIFine: true,
+  bestDay: new Date()
+})
 
 warnLogger('main - warn', {
-	desc: 'Houston, we have a problem',
-	isWeDieRightNow: false,
-	isDebugTime: true,
-	encouragingKitten: ':3'
+  desc: 'Houston, we have a problem',
+  isWeDieRightNow: false,
+  isDebugTime: true,
+  encouragingKitten: ':3'
 })
 
 errorLogger('main - error', {
-	desc: 'Panic, pls somebody halp!',
-	isAllBad: true
+  desc: 'Panic, pls somebody halp!',
+  isAllBad: true
 })
 
 debugLogger('main - debug', {
-	desc: 'non-essential data',
-	commandArg: {
-		line: 'hello world',
-		startIndex: 0,
-		endIndex: 3
-	},
-	callback: 'hello',
-	error: null
+  desc: 'non-essential data',
+  commandArg: {
+    line: 'hello world',
+    startIndex: 0,
+    endIndex: 3
+  },
+  callback: 'hello',
+  error: null
 })
